ef010ed0cd01928defc58d42b3dd92a0b05fcac8
yogurtd3vil <67793637+yogurtd3vil@users.noreply.github.com>
2023-04-13 11:39:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const moment = require('moment');
 moment.locale('pt-br')
 
-const boostData = moment('2023-03-31T00:34:55.655000+00:00'); //premium_guild_since
-const diff = moment().diff(boostData, 'weeks');
-const boostCalc = boostData.clone().add(boost(diff), 'months');
+const boostData = moment('2022-03-06T17:52:37.877000+00:00'); //premium_guild_since
+const databoost = new Date(boostData);
+const dataAtual = new Date();
+const diff = dif(databoost, dataAtual);
+const boostCalc = boostData.clone().add(boost(diff.week), 'months');
 const boostMS = boostCalc.diff(moment(), true);
 const boostinfo = moment.duration(boostMS);
 const unixTimestamp = Math.floor(boostCalc.valueOf() / 1000); // unix <t:${unixTimestamp}:R>
@@ -12,22 +14,71 @@ const unixTimestamp = Math.floor(boostCalc.valueOf() / 1000); // unix <t:${unixT
 console.log(`faltam ${boostinfo.months()} meses, ${boostinfo.days()} dias e ${boostinfo.hours()} horas para o próximo up`); //tempo restante
 console.log(`irá upar em: ${boostCalc.format("D [de] MMMM [de] YYYY [às] HH:mm")}`); //data exata
 
-function boost(mes) {
-  if (mes >= 24) {
-    return 24;
-  } else if (mes >= 18) {
-    return 18;
-  } else if (mes >= 15) {
-    return 15;
-  } else if (mes >= 12) {
-    return 12;
-  } else if (mes >= 9) {
-    return 9;
-  } else if (mes >= 6) {
-    return 6;
-  } else if (mes >= 3) {
-    return 3;
-  } else {
-    return 2;
+function boost(time) {
+  let badge;
+  switch (time) {
+    case 0:
+    case 1:
+      badge = 2;
+      break;
+    case 2:
+      badge = 3;
+      break;
+    case 3:
+    case 4:
+    case 5:
+      badge = 6;
+      break;
+    case 6:
+    case 7:
+    case 8:
+      badge = 9;
+      break;
+    case 9:
+    case 10:
+    case 11:
+      badge = 12;
+      break;
+    case 12:
+    case 13:
+    case 14:
+      badge = 15;
+      break;
+    case 15:
+    case 16:
+    case 17:
+      badge = 18;
+      break;
+    case 18:
+    case 19:
+    case 20:
+    case 21:
+    case 22:
+    case 23:
+      badge = 24;
+      break;
+    default:
+      if (time >= 24) {
+        badge = 24
+      }
   }
+  return badge;
 }
+
+function dif(date1, date2) {
+  const segundos = Math.floor((date2 - date1) / 1000);
+  const minutos = Math.floor(segundos / 60);
+  const horas = Math.floor(minutos / 60);
+  const dias = Math.floor(horas / 24);
+
+  const diff = {
+    sec: segundos % 60,
+    min: minutos % 60,
+    hour: horas % 24,
+    day: dias % 31,
+    week: Math.floor(dias / 31),
+  };
+  
+  return diff;
+}
+
